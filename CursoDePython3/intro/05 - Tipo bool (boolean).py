1afff6b20d61f95fce091d1480689251f5c9c623
JulianoSilva05 <julianoqm@gmail.com>
2025-05-07 13:08:52 -0300

    tipos_dados

---
+++ CursoDePython3/intro/05 - Tipo bool (boolean).py
@@ -0,0 +1,23 @@
+# Tipo de dado bool (bolleano)
+# Ao questionar algo em um programa, só existem duas respostas possíveis: True ou False.
+print(10 > 5)  # True
+print(10 == 5)  # False
+print(10 < 5)  # False
+print( type(10 < 5) )
+# sim (True) ou não (False).
+# Existe vários operadores para "questionar".capitalize
+# Dentre eles o ==, que é um operador lógico que questiuona se um valor é igual a outro
+# Tipos primitivos são os tipos de dados mais básicos e fundamentais em uma linguagem de programação, como int, float, str e bool.
+# Na prática, eles representam os blocos de construção para manipular dados simples:
+# - int: números inteiros, como 1, 2, 3.
+# - float: números decimais, como 1.5, 3.14.
+# - str: cadeias de caracteres (texto), como "Olá", "Python".
+# - bool: valores lógicos, True ou False, usados para decisões e condições.
+
+# Tipos não primitivos, por outro lado, são estruturas de dados mais complexas que podem ser compostas por tipos primitivos.
+# Exemplos incluem listas, tuplas, dicionários e conjuntos:
+# - list: uma coleção ordenada e mutável de itens, como [1, 2, 3].
+# - tuple: uma coleção ordenada e imutável de itens, como (1, 2, 3).
+# - dict: uma coleção de pares chave-valor, como {"nome": "Juliano", "idade": 25}.
+# - set: uma coleção não ordenada de itens únicos, como {1, 2, 3}.
+
