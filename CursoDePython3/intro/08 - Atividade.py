3cdd2713a798f09384a9acee826444be270a12ae
JulianoSilva05 <julianoqm@gmail.com>
2025-05-08 22:58:18 -0300

    atividade08

---
+++ CursoDePython3/intro/08 - Atividade.py
@@ -0,0 +1,13 @@
+nome = "Juliano"
+sobrenome = "Silva"
+idade = 35
+ano_nascimento = 1989
+maior_de_idade = True
+altura_metros = 1.74
+
+print("Nome: ", nome)
+print("Sobrenome: ", sobrenome)
+print("Idade: ", idade)
+print("Ano de nascimento: " ,ano_nascimento )
+print("É maior de idade: " , maior_de_idade)
+print("Altura em metros: " , altura_metros)
