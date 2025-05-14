43230d15342b6f3c56dd4d41fb33f816390f2760
JulianoSilva05 <julianoqm@gmail.com>
2025-05-14 00:01:36 -0300

    format

---
+++ CursoDePython3/intro/12 - Atividade.py
@@ -0,0 +1,7 @@
+nome = "Juliano"
+altura = 1.74
+peso = 79
+imc = peso / altura ** 2
+print(nome, " tem ", altura,"m de altura,")
+print("pesa ", peso, "Kg")
+print("e seu imc é ",imc)
---
+++ CursoDePython3/intro/13 - Atividade.py
@@ -0,0 +1,7 @@
+nome = "Juliano"
+altura = 1.74
+peso = 79
+imc = peso / altura ** 2
+print(f"{nome} tem {altura:.2f}m de altura,")
+print(f"pesa {peso}Kg")
+print(f"e seu imc é {imc:.2f}")
